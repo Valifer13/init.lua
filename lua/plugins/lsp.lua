4bd4b76c07557831df96a5617dd6f98c991bce70
Valifer13 <vianviszello@gmail.com>
2025-11-20 02:46:36 +0800

    make adjustment

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,25 @@
+vim.diagnostic.config({
+    virtual_text = true,       -- Teks error di dalam kode
+    signs = true,              -- Icon E / W
+    underline = true,
+    update_in_insert = false,  -- recommended
+    severity_sort = true,
+    -- float = {
+    --     show_header = true,
+    --     border = "rounded",
+    --     source = "always",
+    --     focusable = false,
+    -- },
+})
+
+-- vim.cmd([[
+--   autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focus = false })
+-- ]])
+
+vim.o.updatetime = 250
+
 return {
-    { 
+    {
         "neovim/nvim-lspconfig",
     },
     {
